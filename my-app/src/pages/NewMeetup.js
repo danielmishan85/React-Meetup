5e93256885791901949d592160273652f36e2ef9
danielmishan85 <danielmishan85@gmail.com>
2022-12-04 23:06:53 +0200

    connect the app to http for using database

--- my-app/src/pages/NewMeetup.js
+++ my-app/src/pages/NewMeetup.js
@@ -1,10 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  const history = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-meetups-9e22f-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history('/');
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
