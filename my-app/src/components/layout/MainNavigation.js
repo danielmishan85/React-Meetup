9eb2477063db802b90f790dab63f60fcbd1acbcc
danielmishan85 <danielmishan85@gmail.com>
2022-12-05 01:30:39 +0200

    use context in components

--- my-app/src/components/layout/MainNavigation.js
+++ my-app/src/components/layout/MainNavigation.js
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
+import FavoritesContext from '../../store/favorite-context';
+
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>Recact Meetups</div>
@@ -13,7 +18,12 @@ function MainNavigation() {
             <Link to='/new-meetup'>New Meetup</Link>
           </li>
           <li>
-            <Link to='/favorites'>Favorites</Link>
+            <Link to='/favorites'>
+              Favorites
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
